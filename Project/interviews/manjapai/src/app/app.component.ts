fcc7a85a021920e3c5bf92edbefcc5d5119f6904
RKG <radhacreative@gmail.com>
2018-09-18 11:45:42 +0530

    profile-page-design

---
+++ Project/interviews/manjapai/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'manjapai';
+  faUser = faUser;
+}
